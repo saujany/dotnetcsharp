86b1087f7d95706d72201c90ef936047cb0b1cb6
saujanya <s_vastav2000@yahoo.com>
2024-05-27 13:55:10 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    
+    public static string rev(string s)
+    {
+        char[]str=s.ToCharArray();
+        Array.Reverse(str);
+        foreach(var ob in str)
+        {
+        Console.WriteLine(ob);
+        }
+       string b=new string(str);
+        return b;
+    
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Enter the string");
+        string str1=Console.ReadLine();
+        string str2=rev(str1);
+        //if(str1.CompareTo(str2)==0)
+        if(str1.Equals(str2))
+        {
+            Console.WriteLine ("it is Anagram");
+            
+        }
+         else
+        
+        {
+            Console.WriteLine ("not Anagram");
+            
+        }
+        Console.ReadLine();
+        }
+    }
